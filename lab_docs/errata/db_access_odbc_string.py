4e18528dbe1fca0d9b7a95178e13f0b9c4d23413
Chad Harper <chad.harper@gmail.com>
2020-06-09 20:58:03 -0400

    Add files via upload
    Updated to include some vidcast stuff

--- lab_docs/errata/db_access_odbc_string.py
+++ lab_docs/errata/db_access_odbc_string.py
@@ -30,46 +30,54 @@ import matplotlib.pyplot as plt
 
 print('Running...')
 # Connect to the server / database
-driver = '{SQL Server}'
+driver = 'SQL Server Native Client 11.0'
 server = '.'                # Change to your server name (ist-s-students.syr.edu for Remote Lab)
-database = 'Stitch'         # Change this to your database name (IST659_M408_netid)
+database = 'vidcast'         # Change this to your database name (ie IST659_M408_netid)
 
 use_windows_auth = True     # Change to False if you don't want to use integrated authentication
 uid = ' '                   # The SQL User ID (leave empty if windows auth)
-pwd = ' '                   # The SQL Password (leave empty if windows auth)
+pwd = ' '                   # The SQL Password (leave empty if windows auth) 
 
 auth = ''
 if use_windows_auth:
-    auth = 'trusted_connection=true'
+    auth = 'trusted_connection=yes'
 else:
     auth = 'uid=%s;pwd=%s' % (uid, pwd)
 
 connect_string = 'DRIVER=%s;SERVER=%s;DATABASE=%s;%s' % (driver, server, database, auth)
-print(connect_string) # DRIVER={SQL Server};SERVER=.;DATABASE=Stitch;trusted_connection=true
+print(connect_string) # DRIVER={SQL Server};SERVER=.;DATABASE=vidcast;trusted_connection=yes
 
 # Step 1: Connect to the server
 conn = pyodbc.connect(connect_string)
 
 # Step 2: Figure out your SQL
 sql = """
-SELECT * FROM Stitch_StitchPost
+SELECT
+  vc_VidCast.vc_VidCastID
+, vc_VidCast.VidCastTitle
+, DATEPART(dw, StartDateTime) as StartDayOfWeek
+, DATEDIFF(n, StartDateTime, EndDateTime) as ActualDuration
+, ScheduleDurationMinutes
+, vc_User.vc_UserID
+, vc_User.UserName
+FROM vc_VidCast
+JOIN vc_User ON vc_User.vc_UserID = vc_VidCast.vc_UserID
  """    # Enter your SQL query between the """ """
 
 # Step 2.5: Execute that SQL
 data = pandas.read_sql(sql, conn)
-
+print(data)
 # Step 3: Do something with whatever came back
 # 3a This line counts the number of posts per category
-category_summary = data.groupby('CategoryName').agg({'StitchPostID' : 'count'})
+daily_summary = data.groupby('StartDayOfWeek').agg({'vc_VidCastID' : 'count'})
 
 # 3b Make a chart out of the summary (bar chart)
-category_chart = category_summary.plot.bar(rot=0)
+daily_chart = daily_summary.plot.bar(rot=0)
 
 # 3c Show the chart
 plt.show()
 
-
 # Step 4: Show the user what they need
-print(category_summary)
+print(daily_summary)
 
 # Step 5: Profit
